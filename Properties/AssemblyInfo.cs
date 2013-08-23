3b3ecbb53ccc8c7b7e595093db778d6a76d8030c
nblumhardt <nblumhardt@nblumhardt.com>
2013-08-24 06:39:35 +1000

    Moved Serilog.Web to Serilog.Extras.Web - following the long-term naming scheme we ended up with in Autofac.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Web;
+using Serilog.Extras.Web;
+
+[assembly: AssemblyTitle("Serilog.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Nicholas Blumhardt 2013")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: PreApplicationStartMethod(typeof(ApplicationLifecycleModule), "Register")]
