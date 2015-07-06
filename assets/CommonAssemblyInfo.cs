d826927b2c61128d066ca4dbad233dee211d24c5
Konstantin Erman <konste@hotmail.com>
2015-07-06 10:42:04 -0700

    Token $version$ is not defined for TeamCity. Sticking NuGet package version right into .nuspec

--- assets/CommonAssemblyInfo.cs
+++ assets/CommonAssemblyInfo.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.1.1.1")]
-[assembly: AssemblyInformationalVersion("2.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
