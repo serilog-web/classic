0d48326962d4f465705d91a4fe398b6df5242c8e
Chui Tey <chui.tey@readify.net>
2016-01-10 13:01:33 +1000

    Set log level to Error when Status Code is 400-500

--- src/SerilogWeb.Classic/Classic/ApplicationLifecycleModule.cs
+++ src/SerilogWeb.Classic/Classic/ApplicationLifecycleModule.cs
@@ -159,7 +159,9 @@ namespace SerilogWeb.Classic
                         return;
 
                     var error = application.Server.GetLastError();
-                    var level = error != null ? LogEventLevel.Error : _requestLoggingLevel;
+                    var level = error != null || 
+                        (application.Response.StatusCode >= 400 && application.Response.StatusCode <= 599) 
+                        ? LogEventLevel.Error : _requestLoggingLevel;
 
                     var logger = Logger;
                     if (ShouldLogFormData)
