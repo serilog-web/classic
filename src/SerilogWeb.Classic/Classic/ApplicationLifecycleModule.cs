9d98455be7375853dc2f93c8c703241bed65fa55
Raj Mahich <mahichr@gmail.com>
2017-06-13 03:59:00 -0400

    Added logic to check AllErrors in Context when LogEventLevel is Error (#39)
    Added logic to check Error collection in Context when LogEventLevel is Error

--- src/SerilogWeb.Classic/Classic/ApplicationLifecycleModule.cs
+++ src/SerilogWeb.Classic/Classic/ApplicationLifecycleModule.cs
@@ -201,6 +201,11 @@ namespace SerilogWeb.Classic
                     var error = application.Server.GetLastError();
                     var level = error != null || application.Response.StatusCode >= 500 ? LogEventLevel.Error : _requestLoggingLevel;
 
+                    if (level == LogEventLevel.Error && error == null && application.Context.AllErrors != null)
+                    {
+                        error = application.Context.AllErrors.LastOrDefault();
+                    }
+
                     var logger = Logger;
                     if (logger.IsEnabled(_formDataLoggingLevel) && FormLoggingStrategy(application.Context))
                     {
