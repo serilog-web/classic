8590fd94c98794353826e901c5160c57ef83185a
Thibaud Desodt <tibo.desodt@gmail.com>
2018-05-21 11:32:48 +0200

    Minor edits to remember how the project is versionned

--- assets/CommonAssemblyInfo.cs
+++ assets/CommonAssemblyInfo.cs
@@ -1,5 +1,7 @@
 using System.Reflection;
 
+// IMPORTANT : do not change the values below
+// they are used in Build.ps1 to do search/replace of version numbers
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.1.1.1")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
